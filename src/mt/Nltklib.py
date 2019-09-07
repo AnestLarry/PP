8ef3228771754e166db5e4572cc5d36655cb2841
Anest <anest@66ws.cc>
2019-09-06 21:33:01 -0400

    delete

--- src/mt/Nltklib.py
+++
@@ -1,8 +0,0 @@
-import nltk
-import jieba
-
-def Get_FreqDist(keystr='',language_temp='en'):
-	if language=='en':
-		return nltk.FreqDist(nltk.WordPunctTokenizer().tokenize(keystr))
-	if language=='zh':
-		return nltk.FreqDist(list(jieba.cut(s)))

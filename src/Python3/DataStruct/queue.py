cd6c099778e98bf62d7d8465041ff753af10d7db
Anest <anest@66ws.cc>
2020-05-27 10:35:14 -0400

    add PriorityQueue

--- src/Python3/DataStruct/queue.py
+++ src/Python3/DataStruct/queue.py
@@ -20,7 +20,7 @@ class Queue:
         if self.__front is None:
             self.__front = self.__rear = self.Node(data)
         else:
-            self.__rear.next = self.Node(data,self.__rear)
+            self.__rear.next = self.Node(data, self.__rear)
             self.__rear = self.__rear.next
         self.__len += 1
 
@@ -31,12 +31,54 @@ class Queue:
             node = self.__front
             node.prev = None
             self.__front = node.next
-            self.__len -=1
+            self.__len -= 1
             return node.data
 
-    def print(self,sep=' ',end="\n"):
+    def print(self, sep=' ', end="\n"):
         cur = self.__front
         while cur:
-            print(cur.data,end=sep)
+            print(cur.data, end=sep)
             cur = cur.next
+        print(end=end)
+
+
+class PriorityQueue:
+    def __init__(self, isMaxPriorityQueue):
+        self.__array = []
+        self.__isMaxPriorityQueue = isMaxPriorityQueue
+
+    def __len__(self):
+        return len(self.__array)
+
+    def is_empty(self):
+        return self.__array is None
+
+    def Enqueue(self, data):
+        if type(data) == "int":
+            self.__array.append(data)
+            i = len(self.__array)>>1
+            while i>-1:
+                if self.__array[i] > 
+        else:
+            raise ValueError("PriorityQueue -> Enqueue(): data is error.")
+        # if self.__front is None:
+        #     self.__front = self.__rear = self.Node(data)
+        # else:
+        #     self.__rear.next = self.Node(data, self.__rear)
+        #     self.__rear = self.__rear.next
+        # self.__len += 1
+
+    def Dequeue(self):
+        if self.__front is None:
+            raise LookupError("Empty Queue.")
+        else:
+            node = self.__front
+            node.prev = None
+            self.__front = node.next
+            self.__len -= 1
+            return node.data
+
+    def print(self, sep=' ', end="\n"):
+        for i in self.__array:
+            print(i, end=sep)
         print(end=end)
